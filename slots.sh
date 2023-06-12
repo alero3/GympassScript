d5b3828d3fd73805c555456f8a39fdf88f1e9fa1
Alessandro Rota <alessandro.rota.i.am@gmail.com>
2023-06-13 00:43:06 +0200

    Update and rename slots to slots.sh

---
+++ slots.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl --location 'https://www.gympass.com/end-user/es/api/booking/gyms/500194/slots?from=2023-06-16T20%3A00%3A00%2B02%3A00&to=2023-06-16T21%3A59%3A59%2B02%3A00&status=1&isCoreIntegration=false' \
+--header 'authority: www.gympass.com' \
+--header 'accept: application/json, text/plain, */*' \
+--header 'accept-language: en-GB,en-US;q=0.9,en;q=0.8' \
+--header 'content-type: application/json' \
+--header 'cookie: _hjSessionUser_2488048=eyJpZCI6IjUyNmM2MTRiLTE5M2QtNTA0NC04OTQ2LTNiNjBhMDVkZWEwNiIsImNyZWF0ZWQiOjE2NjIzNjk1NDY5NjIsImV4aXN0aW5nIjp0cnVlfQ==; drift_aid=96e82b3e-2a87-4682-9c38-40ad2df0a831; driftt_aid=96e82b3e-2a87-4682-9c38-40ad2df0a831; locale=en_IE; did=_oI5Nd5KvPm4YNdMEbyCtkIZYl7eWAzcKGVDhEuna74; viewer_id=M26iqR_mVfxZEtdWICGPhAIBSnG94UP8h57uOMP34Vs; _gcl_au=1.1.1720397324.1677088058; _rdt_uuid=1677088058237.a99a3c92-fa5d-4d65-91c5-1a2d042a23f9; _fbp=fb.1.1677088058402.1288860129; _tt_enable_cookie=1; _ttp=syo3KUgyH8HyQ3v2Y4F-P0A7vog; afUserId=e2423847-001c-4b9b-ac79-0049d8e640ca-p; hubspotutk=f26b7715c8d5670293a2dc4e37dcf245; user_identify=alessandro-rota; email=ZjEwUG9pTkluaHpwaTByT3U4ZVNUM0Z6QTZqeTB0cnBZL0RVQ3NNT3V3Q3ZBUlV1WWFFYzlwODRzbEhwVU4xTy0tV1AxV3g5TzI5NW9SanV0VlYxL2Jqdz09--05af71fa51697645b978e130b01074dda8f301cc; has_logged_in=1; sp=5643b719-9090-4712-8569-64930204fbcc; optimizelyEndUserId=oeu1677088286842r0.3770940851077209; ajs_anonymous_id=34ada43b-dd0a-4005-9dc5-1f3d080a9f1
